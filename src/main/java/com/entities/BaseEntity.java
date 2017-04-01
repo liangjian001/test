74f5be4bf5f574b77f09344f6c949c658245a9c4
liangjian001 <294566692@qq.com>
2017-04-01 10:52:06 +0800

    创建类

---
+++ src/main/java/com/entities/BaseEntity.java
@@ -0,0 +1,65 @@
+package com.entities;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable{
+
+	private static final long serialVersionUID = -6303546708813719329L;
+	
+	private String id;
+	
+	private String createId;
+
+    private String createDate;
+
+    private String createTime;
+
+    private String updateId;
+
+    private String updateDate;
+
+    private String updateTime;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCreateId() {
+		return createId;
+	}
+	public void setCreateId(String createId) {
+		this.createId = createId;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateId() {
+		return updateId;
+	}
+	public void setUpdateId(String updateId) {
+		this.updateId = updateId;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+}
--- src/main/java/com/entities/User.java
+++ src/main/java/com/entities/User.java
@@ -1,5 +1,51 @@
 package com.entities;
 
-public class User {
-
+public class User extends BaseEntity{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3379323299050562463L;
+	
+	private String id;
+	private String userCode;
+	private String userName;
+	private String password;
+	private String email;
+	private String state;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
 }
